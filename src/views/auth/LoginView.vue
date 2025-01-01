2505847c2b957788129ec62f6174d923b16ec9e9
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-01-01 10:19:55 -0300

    Auth Login layout

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -12,7 +12,51 @@ axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(() => {
 </script>
 
 <template>
-  <h1>Login</h1>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+       data-sidebar-position="fixed" data-header-position="fixed">
+    <div
+      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+      <div class="d-flex align-items-center justify-content-center w-100">
+        <div class="row justify-content-center w-100">
+          <div class="col-md-8 col-lg-6 col-xxl-3">
+            <div class="card mb-0">
+              <div class="card-body">
+                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                  <img src="@/assets/modernize/images/logos/dark-logo.svg" width="180" alt="">
+                </a>
+                <p class="text-center">Seu controle de livros</p>
+                <form>
+                  <div class="mb-3">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-4">
+                    <label for="password" class="form-label">Senha</label>
+                    <input type="password" class="form-control" id="password">
+                  </div>
+                  <div class="d-flex align-items-center justify-content-between mb-4">
+                    <div class="form-check">
+<!--                      <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>-->
+<!--                      <label class="form-check-label text-dark" for="flexCheckChecked">-->
+<!--                        Remeber this Device-->
+<!--                      </label>-->
+                    </div>
+                    <a class="text-primary fw-bold" href="./index.html">Esqueceu sua senha ?</a>
+                  </div>
+                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Entrar</a>
+                  <div class="d-flex align-items-center justify-content-center">
+                    <p class="fs-4 mb-0 fw-bold">Novo por aqui?</p>
+                    <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Crie uma conta</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
