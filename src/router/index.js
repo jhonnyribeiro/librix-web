65c178bea53d755542f65c4a9731c921b22b4414
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-01-01 08:17:43 -0300

    create LoginView

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/entrar',
+      name: 'auth.login',
+      component: import('../views/auth/LoginView.vue'),
+    },
+    {
       path: '/',
       name: 'dashboard',
       component: import('../views/DashboardView.vue'),
