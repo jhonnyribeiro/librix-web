d8e2cf321f156b9fbe4e6fa4762ea86269e39179
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-01-01 10:15:31 -0300

    login test

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,16 +1,18 @@
 <script setup>
-
 import axios from 'axios'
 
-axios.defaults.withCredentials=true;
-axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
-
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
+axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(() => {
+  axios.post('http://127.0.0.1:8000/api/login',{
+    email: 'test@example.com',
+    password: 'password'
+  })
+})
 </script>
 
 <template>
   <h1>Login</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
